2ffbeecb054a28cfc95fd7147a8ead8c18f26d11
Tobias Pinnekamp <tobias.pinnekamp@eshaker.de>
2019-06-18 11:26:25 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Link from 'next/link'
+import Layout from '../components/Layout'
+import { NextPage } from 'next'
+
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello Next.js 👋</h1>
+      <p>
+      test
+      </p>
+    </div>
+  )
+}
+
+export default IndexPage
